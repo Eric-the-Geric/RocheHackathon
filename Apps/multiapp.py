7c946c130f52297198ab350ec9c29f7708484953
afrodisac <eric.gericke2@gmail.com>
2023-10-07 10:09:57 +0200

    added skeleton for app

---
+++ Apps/multiapp.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+
+class MultiApp:
+    """ Class to add multiple streamlit applications together to create
+    multiple pages
+
+    Useage: 
+        def foo():
+            st.title("Hello foo")
+        def bar():
+            st.title("hello bar...")
+
+        app = MultiApp()
+        app.add_app("Foo", foo)
+        add.add_app("Bar", bar)
+
+        app.run()
+
+    """
+
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        """ Adds a new app 
+        Params:
+            func: the python function to render this app 
+
+            title:
+                the title of the app. This is what will appear in the dropdown
+
+        """
+        self.apps.append({
+            "title": title,
+            "function": funct
+            })
+
+    def run(self):
+        app = st.selectbox('Naviagtion',
+                           self.apps,
+                           format_func=lamda app: app['title'])
+        app['function']()

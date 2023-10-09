895e3715e08219bf22163441c299321db3e37029
Eric <eric.gericke2@gmail.com>
2023-10-09 20:02:37 +0200

    finishing touches +renaming

--- App/apps/model.py
+++ App/apps/model.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pickle
 import pandas as pd
@@ -23,57 +24,59 @@ def app():
                 """)
     
     st.sidebar.write("1. Choose the cleaned dataset")
-    csv = st.sidebar.file_uploader("select the dataset to train the model on")
+    data_path = st.sidebar.selectbox("select the training data", os.listdir("data/"))
+    df = pd.read_csv("data/" +data_path)
+    #csv = st.sidebar.file_uploader("select the dataset to train the model on")
     trained_model = False
     saved = False
 
-    if csv != None and not saved:
-        name = st.text_input("What do you want to call your model?")
+    #if csv != None and not saved:
+    name = st.text_input("What do you want to call your model?")
 
-        # Read in the CSV
-        df = pd.read_csv(csv)
+    # Read in the CSV
+    #df = pd.read_csv(csv)
 
-        # SPlit the data for train_test_split
-        df_x = df.iloc[:,:-1]
-        df_y = df.iloc[:, -1]
-        X_train, X_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=42)
+    # SPlit the data for train_test_split
+    df_x = df.iloc[:,:-1]
+    df_y = df.iloc[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=42)
 
 
-        st.sidebar.write("2. Select a model")
-        models = [{"model": "gxboost", "object": XGBClassifier}, {"model": "forest", "object": RandomForestClassifier}, {"model": "Naive Bayes", "object": GaussianNB}]
-        model = st.sidebar.selectbox('select a model to train', models, format_func=lambda model: model['model'])
-       
+    st.sidebar.write("2. Select a model")
+    models = [{"model": "gxboost", "object": XGBClassifier}, {"model": "forest", "object": RandomForestClassifier}, {"model": "Naive Bayes", "object": GaussianNB}]
+    model = st.sidebar.selectbox('select a model to train', models, format_func=lambda model: model['model'])
+   
 
-        st.sidebar.write("3. Select a normalisation strategy")
-        normalisation = [{"norm": "minmax", "object": MinMaxScaler}, {"norm": "StandardScaler", "object": StandardScaler}]
-        norm = st.sidebar.selectbox('select a model to train', normalisation, format_func=lambda norm: norm['norm'])
+    st.sidebar.write("3. Select a normalisation strategy")
+    normalisation = [{"norm": "minmax", "object": MinMaxScaler}, {"norm": "StandardScaler", "object": StandardScaler}]
+    norm = st.sidebar.selectbox('select a model to train', normalisation, format_func=lambda norm: norm['norm'])
 
-        # Upsampling seems to do much better than downsampling
-        smote = SMOTE(sampling_strategy="auto", random_state=42)
-        X_train, y_train = smote.fit_resample(X_train, y_train)
+    # Upsampling seems to do much better than downsampling
+    smote = SMOTE(sampling_strategy="auto", random_state=42)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
 
-        # Now create a pipeline that we can save for inference
-        pipe = make_pipeline(norm['object'](),model['object']() )
-        pipe.fit(X_train, y_train)
+    # Now create a pipeline that we can save for inference
+    pipe = make_pipeline(norm['object'](),model['object']())
+    pipe.fit(X_train, y_train)
 
-        if st.button("show accuracy"):
-            st.write(round(pipe.score(X_test, y_test), 2))
-            st.write(classification_report(pipe.predict(X_test), y_test))
+    if st.button("show accuracy"):
+        st.write(round(pipe.score(X_test, y_test), 2))
+        st.dataframe(classification_report(pipe.predict(X_test), y_test, output_dict=True))
 
-            cm = confusion_matrix(pipe.predict(X_test), y_test)
-            plot_confusion_matrix(cm)
-            trained_model = True
+        cm = confusion_matrix(pipe.predict(X_test), y_test)
+        plot_confusion_matrix(cm)
+        trained_model = True
 
-        if st.button("show feature importance"):
-            feature_importances = permutation_importance(pipe, X_test, y_test, n_repeats=10, random_state=42)
-            features = df_x.columns.to_list()
-            plot_importance(features, feature_importances.importances_mean)
+    if st.button("show feature importance"):
+        feature_importances = permutation_importance(pipe, X_test, y_test, n_repeats=10, random_state=42)
+        features = df_x.columns.to_list()
+        plot_importance(features, feature_importances.importances_mean)
 
-        if trained_model:
-            download =  st.download_button("download the model", data=pickle.dumps(pipe), file_name=f"{name}.pkl")
-            if download:
-                saved = True
-                st.write("congrats, the model was saved")
+    if trained_model:
+        download =  st.download_button("download the model", data=pickle.dumps(pipe), file_name=f"{name}.pkl")
+        if download:
+            saved = True
+            st.write("congrats, the model was saved")
 
 def plot_confusion_matrix(cm):
         fig = plt.figure(figsize=(6, 6))
